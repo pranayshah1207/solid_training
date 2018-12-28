bd33b06a1df7652ac4207c854f1fbd8d78a08797
Pranay Shah <pranays@Meditab.Local>
2018-12-28 18:26:43 +0530

    No change in this

--- SRP/src/com/meditab/training/srp/exercise/CarManager.java
+++ SRP/src/com/meditab/training/srp/exercise/CarManager.java
@@ -1,44 +1,50 @@
-package com.meditab.training.srp.exercise;
+package day5p1;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class CarManager
-{
-    private List<Car> _carsDb = Arrays
-        .asList(new Car("1", "Golf III", "Volkswagen"), new Car("2", "Multipla", "Fiat"),
-            new Car("3", "Megane", "Renault"));
+public class CarManager {
 
-    public Car getFromDb(final String carId)
-    {
-        for (Car car : _carsDb) {
-            if (car.getId().equals(carId)) {
-                return car;
-            }
-        }
-        return null;
-    }
+	private List<Car> _carsDb = Arrays.asList(new Car("1", "Golf III", "Volkswagen"), new Car("2", "Multipla", "Fiat"),
+			new Car("3", "Megane", "Renault"));
 
-    public String getCarsNames()
-    {
-        StringBuilder sb = new StringBuilder();
-        for (Car car : _carsDb) {
-            sb.append(car.getBrand());
-            sb.append(" ");
-            sb.append(car.getModel());
-            sb.append(", ");
-        }
-        return sb.substring(0, sb.length() - 2);
-    }
+	public Car getFromDb(final String carId) {
+		for (Car car : _carsDb) {
+			if (car.getId().equals(carId)) {
+				return car;
+			}
+		}
+		return null;
+	}
 
-    public Car getBestCar()
-    {
-        Car bestCar = null;
-        for (Car car : _carsDb) {
-            if (bestCar == null || car.getModel().compareTo(bestCar.getModel()) > 0) {
-                bestCar = car;
-            }
-        }
-        return bestCar;
-    }
+	public String getCarsNames() {
+		StringBuilder sb = new StringBuilder();
+		for (Car car : _carsDb) {
+			sb.append(car.getBrand());
+			sb.append(" ");
+			sb.append(car.getModel());
+			sb.append(", ");
+		}
+		return sb.substring(0, sb.length() - 2);
+	}
+
+	public List<Car> getBestCar() {
+		return _carsDb;
+	}
+}
+
+class BestCar {
+	Car bestCar = null;
+	CarManager cm = null;
+	List<Car> lc = cm.getBestCar();
+
+	public Car getBestCar() {
+
+		for (Car car : lc) {
+			if (bestCar == null || car.getModel().compareTo(bestCar.getModel()) > 0) {
+				bestCar = car;
+			}
+		}
+		return bestCar;
+	}
 }
